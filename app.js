e97a7694e95e898fe66426634e2118fa18b85080
Mashudu <kcirdrol@gmail.com>
2020-06-23 22:29:24 +0200

    update app.js and post page

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 const FacebookStrategy = require("passport-facebook").Strategy;
+const multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 const app = express();
+var router = express.Router();
 
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -28,16 +31,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/backroomDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
+// app.use(multer({ dest: "uploads/",
+//  rename: function (fieldname, filename) {
+//    return filename;
+//  },
+// }));
 
 const postSchema = new mongoose.Schema ({
   location: String,
   price: Number,
-  roomPic: String,
   occupation: String,
   gender: String,
-  singleOrSharing: String
+  singleOrSharing: String,
+  roomPic: { data: Buffer, contentType: String },
+  description: String
 });
 
 const Post = mongoose.model("Post", postSchema);
@@ -94,7 +103,11 @@ passport.use(new FacebookStrategy({
 ));
 
 app.get("/", function(req, res){
-res.render("index");
+  Post.find({}, function(err, posts){
+    res.render("index", {
+      posts: posts
+      });
+  });
 });
 
 app.get("/auth/google",
@@ -143,6 +156,55 @@ app.get("/contact", function(req, res){
   res.render("contact");
 });
 
-app.listen(3000, function() {
+app.post("/post", function(req, res) {
+  const post = new Post({
+    location: req.body.location,
+    price: req.body.price,
+    occupation: req.body.occupation,
+    gender: req.body.gender,
+    singleOrSharing: req.body.singleOrSharing,
+    roomPic: req.body.roomPic,
+    description: req.body.description
+    });
+
+    post.save(function(err){
+      if (!err){
+          res.redirect("/");
+      }
+    });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      location: req.body.location,
+      price: req.body.price,
+      occupation: req.body.occupation,
+      gender: req.body.gender,
+      singleOrSharing: req.body.singleOrSharing,
+      roomPic: req.body.roomPic,
+      description: req.body.description
+    });
+  });
+
+});
+app.post("/api/photo", function(req,res){
+ var newItem = new Item();
+ newItem.img.data = fs.readFileSync(req.files.userPhoto.path)
+ newItem.img.contentType = 'image/png';
+ newItem.save();
+});
+
+// app.post('/post', upload.single('avatar'), function (req, res, next) {
+//   // req.file is the `avatar` file
+//   // req.body will hold the text fields, if there were any
+// })
+
+;
+
+app.listen(4000, function() {
   console.log("Server started on port 3000");
 });
